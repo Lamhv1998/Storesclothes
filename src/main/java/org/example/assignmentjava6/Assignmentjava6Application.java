b6c659fee7286dd4e7b649a62cf9ead63cf93873
devlam <havulam1998z@gmail.com>
2025-03-26 15:59:18 +0700

    first commit

---
+++ src/main/java/org/example/assignmentjava6/Assignmentjava6Application.java
@@ -0,0 +1,13 @@
+package org.example.assignmentjava6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignmentjava6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignmentjava6Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assignmentjava6/Assignmentjava6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignmentjava6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignmentjava6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
